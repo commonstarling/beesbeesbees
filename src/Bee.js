6d84969777f4339aba95b4a2d12a794f75c7d4f6
Sheena Ramirez <sheenaramirez2@gmail.com>
2017-05-22 12:26:46 -0700

    "Change functionality in grub class for bee inheritance; add bee class functionality"

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,14 @@
 var Bee = function() {
+  this.age = 5;
+  this.color  = 'yellow';
+  this.food = this.food('jelly');
+  this.job = this.job('keep on growing');
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+
+Bee.prototype.constructor = Bee;
+
+Bee.prototype.job = function (job){
+  return job;
+};
--- src/Grub.js
+++ src/Grub.js
@@ -1,9 +1,12 @@
 var Grub = function() {
   this.age = 0;
   this.color = 'pink';
-  this.food = 'jelly';
-
+  this.food = this.food('jelly');
 };
 
 Grub.prototype.eat = function (){};
+
+Grub.prototype.food = function (food){
+  return food;
+}
 
