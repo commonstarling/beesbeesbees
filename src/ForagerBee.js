0ce2b91ac1951dff8d44fabe44916f4280e2925b
Sheena Ramirez <sheenaramirez2@gmail.com>
2017-05-22 13:04:09 -0700

    "Add functionality for ForagerBee subclass"

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,24 @@
 var ForagerBee = function() {
+  this.age = 10;
+  this.job = this.job('find pollen');
+  this.color  = this.color('yellow');
+  this.food = this.food('jelly');
+  this.canFly = this.canFly();
+  this.treasureChest = this.treasureChest();
 };
 
+ForagerBee.prototype = Object.create(HoneyMakerBee.prototype);
+
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.canFly = function (){
+  return true;
+};
+
+ForagerBee.prototype.treasureChest = function (){
+  return [];
+};
+
+ForagerBee.prototype.forage = function (){
+  this.treasureChest.push('treasure');
+};
